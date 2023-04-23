5b6f522b1663d1898bba60eea76698a3f3f47c5a
mubarizra@code.edu.az <mubarizra@code.edu.az>
2023-04-24 00:23:22 +0400

    Task

---
+++ EntityFramework-Slider/EntityFramework-Slider/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EntityFramework_Slider.Models
+{
+    public class Category:BaseEntity
+    {
+        [Required(ErrorMessage ="Dont be empty")]
+        [StringLength(10,ErrorMessage ="The name length must be 10 characters")]
+        public string Name { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
